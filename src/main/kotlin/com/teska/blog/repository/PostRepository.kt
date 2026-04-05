3ba07e0c520604fd6e771b202df2037097cf8708
Patrycja Teska <teska.patrycja@wp.pl>
2026-04-05 17:23:28 +0200

    - change configuration to save data to file instead of living in-memory

--- src/main/kotlin/com/teska/blog/repository/PostRepository.kt
+++ src/main/kotlin/com/teska/blog/repository/PostRepository.kt
@@ -2,5 +2,7 @@ package com.teska.blog.repository
 
 import com.teska.blog.model.Post
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface PostRepository : JpaRepository<Post, Long>
